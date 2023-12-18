41a94594aa816868a95a5d7543a9e0d2111e8202
GaZ33 <gasinho.ga@gmail.com>
2023-12-17 21:27:50 -0300

    Criando e alterando os diretórios para uma melhor organização, criação da conexão do meu MySQL com o SQLAlchemy

---
+++ market/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+app = Flask(__name__)
+
+# Pegando o valor do .env
+db_connection = os.getenv("DB_CONNECTION")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_connection
+# Desativa o rastreamento de modificações para evitar avisos
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+from market import routes
+
+
